9891e491c3df96768fae25e3cbf16b1cdbc4e418
alikhan3577902 <91456296+alikhan3577902@users.noreply.github.com>
2024-02-21 16:04:25 +0500

    Update SidebarBody.jsx

--- src/components/SidebarBody.jsx
+++ src/components/SidebarBody.jsx
@@ -32,9 +32,10 @@ export default function SidebarBody() {
                 <div className="icons-social">
                     <a href="https://github.com/alikhan3577902" target="_blank" rel="noopener noreferrer"><AiOutlineGithub /></a>
                     <a href="https://wa.me/+923252250078" target="_blank" rel="noopener noreferrer"><AiOutlineWhatsApp /></a>
-                    <a href="https://instagram.com/ahmdsk._" target="_blank" rel="noopener noreferrer"><AiOutlineInstagram /></a>
+                    <a href="#" target="_blank" rel="noopener noreferrer"><AiOutlineInstagram /></a>
                 </div>
                 <h4 className="copyright">Developed by <b>MUHAMMAD ALI</b></h4>
+                <h4 className="copyright">Developed by <b>Ahmad Shaleh</b></h4>
             </div>
         </div>
     )
